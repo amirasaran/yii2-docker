0c8d84a6a5185be3eb884e65bebf48f36b240eaa
Amir Mohsen Asaran <a.asaran@karinaco.com>
2016-07-10 00:23:08 +0430

    add ./init and ./yii commands nginx bug fix add yii2 php requirement to dockerfile change into start.sh and setup.sh

--- web/rootfs/start.sh
+++ web/rootfs/start.sh
@@ -13,13 +13,5 @@ else
         sed -i "s/_BACKEND_SITE_NAME_/localhost/" /etc/nginx/conf.d/backend.conf
 fi
 
-if [[ ! -z ${SITE_MOOD} ]]; then
-	SITE_MOOD=$SITE_MODE
-else
-	SITE_MODE='Production'
-fi
-
-
-composer global require "fxp/composer-asset-plugin:~1.1.1" && composer create-project --prefer-dist yiisoft/yii2-app-advanced /var/www && php7 /var/www/init --env=$SITE_MODE --overwrite=All
 
 /usr/bin/supervisord -n
